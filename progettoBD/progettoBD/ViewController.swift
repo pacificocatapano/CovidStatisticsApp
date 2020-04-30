e66515c99038164da5cb02993e953e801603162c
Salvatore Giugliano <salvatoreg.1898@outlook.it>
2020-04-30 16:27:17 +0200

    test

--- progettoBD/progettoBD/ViewController.swift
+++ progettoBD/progettoBD/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var test = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
