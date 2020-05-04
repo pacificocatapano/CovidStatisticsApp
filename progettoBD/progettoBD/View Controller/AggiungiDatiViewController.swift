d8e4583919468fac5bfdbf792cf5c025306591c2
Pacifico Catapano <pacifico.catapano@gmail.com>
2020-05-04 10:23:58 +0200

    scroll view non funzionanti

---
+++ progettoBD/progettoBD/View Controller/AggiungiDatiViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AggiungiDatiViewController.swift
+//  progettoBD
+//
+//  Created by Pacifico Catapano on 04/05/2020.
+//  Copyright © 2020 Pacifico Catapano. All rights reserved.
+//
+
+import UIKit
+
+class AggiungiDatiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ progettoBD/progettoBD/View Controller/InfoRegioniProvinceViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InfoRegioniProvinceViewController.swift
+//  progettoBD
+//
+//  Created by Pacifico Catapano on 04/05/2020.
+//  Copyright © 2020 Pacifico Catapano. All rights reserved.
+//
+
+import UIKit
+
+class InfoRegioniProvinceViewController: UIViewController {
+
+    @IBOutlet weak var newData: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newData.layer.cornerRadius = 10
+    }
+    
+
+}
