b747f0558df7c4b4a94cb7bdce5b7886571506ec
Salvatore Giugliano <salvatoreg.1898@outlook.it>
2020-05-05 11:49:07 +0200

    fix search bar

--- progettoBD/progettoBD/InfoRegioniProvinceViewController.swift
+++ progettoBD/progettoBD/InfoRegioniProvinceViewController.swift
@@ -22,16 +22,26 @@ class InfoRegioniProvinceViewController: UIViewController, UISearchBarDelegate, 
         super.viewDidLoad()
         newData.layer.cornerRadius = 10
         searchBarInfoRegioniProvince.delegate = self
+        searchBarInfoRegioniProvince.addDoneButton(title: "Close", target: self, selector: #selector(tapDone(sender:)))
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    @objc func tapDone(sender: Any) {
+        searchBarInfoRegioniProvince.text = ""
+        self.view.endEditing(true)
+    }
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        searchBarInfoRegioniProvince.resignFirstResponder()
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         // Do some search stuff
     }
 
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBarInfoRegioniProvince.text = ""
-        searchBarInfoRegioniProvince.endEditing(true)
-    }
     
 }
 
